7dbba97993b506a0f33e991bdc6e35e88b6fd3e2
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 12:22:49 -0700

    Initial-ish commit

---
+++ arrays.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+/** 1. printIndices */
+const printIndices = (items) => {
+	// Replace this with your code
+  for (const i in items) {
+    console.log(items[i], i);
+  }
+  // return;
+};
+
+
+/** 2. everyOtherItem */
+const everyOtherItem = (items) => {
+	// Replace this with your code
+};
+
+
+/** 3. smallestNItems */
+const smallestNItems = (items, n) => {
+	// Replace this with your code
+};
+
+printIndices(['apple', 'berry', 'cherry'])
