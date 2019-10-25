17d1d99b974637c1e85a4d8ff6bead70125f03c1
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 17:13:39 -0700

    WIP: added support for getmelonsatprice

--- objects.js
+++ objects.js
@@ -3,11 +3,41 @@
 
 /** 1. countWords */
 function countWords(phrase) {
-	// Replace this with your code
+	let wordCounts = {};
+
+  const wordsList = phrase.split(' ');
+
+  console.log(wordsList);
+
+  for (const word of wordsList) {
+    if (!wordCounts[word]) {
+      wordCounts[word] = 1;
+    } else {
+      wordCounts[word] += 1;
+    }
+  }
+
+  return wordCounts;
 }
+
+// console.log(countWords("The quick brown fox jumps over the lazy dog and other fox"));
 
 
 /** 2. getMelonsAtPrice */
 function getMelonsAtPrice(price) {
-	// Replace this with your code
+
+  const melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  };
+
+  if (!melonPrices[price]) {
+    return "No melons at that price";
+  } else {
+    return melonPrices[price].sort();
+  }
 }
+
+console.log(getMelonsAtPrice(5.50));
